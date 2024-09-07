9627e3fe51cccd1e97fb01932575c630490a299a
vibhmitra <vibhanshumitra@gmail.com>
2024-09-07 23:42:30 +0530

    Added Dark and Light theme option using astro island and FINISED! 🏁 the course

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -11,7 +11,7 @@ export default function Greeting({messages}) {
       <button onClick={() => setGreeting(randomMessage())} style="margin: 0.5rem;padding: 0.2rem 2rem; font-size: 1rem; font-weight: bold;">
         Click ME!
       </button>
-      <h3 style="font-size: 1rem; display: inline; padding: 0.5rem; background-color: #fff78070;" >{greeting}!  Thank you for visiting!</h3>
+      <h3 style="font-size: 1rem; display: inline; padding: 0.5rem; background-color: #fff78070;" >{greeting}!  Ty for visiting my Island! <span style="font-family: 'Segoe UI Emoji';">🏝</span></h3>
     </div>
   );
 }
