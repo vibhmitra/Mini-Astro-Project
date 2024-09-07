30e23ac16c129b1cb590d6ce839e72a35817c269
vibhmitra <vibhanshumitra@gmail.com>
2024-09-07 22:55:24 +0530

    fixed some styles and started learning island 🏝

---
+++ src/components/Greeting.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'preact/hooks';
+
+export default function Greeting({messages}) {
+
+  const randomMessage = () => messages[(Math.floor(Math.random() * messages.length))];
+
+  const [greeting, setGreeting] = useState(messages[0]);
+
+  return (
+    <div style="background-color: #4d1d9515;">
+      <button onClick={() => setGreeting(randomMessage())} style="margin: 0.5rem;padding: 0.2rem 2rem; font-size: 1rem; font-weight: bold;">
+        Click ME!
+      </button>
+      <h3 style="font-size: 1rem; display: inline; padding: 0.5rem; background-color: #fff78070;" >{greeting}!  Thank you for visiting!</h3>
+    </div>
+  );
+}
