40879fef0abf6f94af956fb0e04f8b2aa408ffaf
vibhmitra <vibhanshumitra@gmail.com>
2024-09-16 14:28:07 +0530

    fixed some styling! ✨🧨

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -1,17 +1,18 @@
-import { useState } from 'preact/hooks';
+import { useState } from "preact/hooks";
 
-export default function Greeting({messages}) {
+export default function Greeting({ messages }) {
+    const randomMessage = () => messages[Math.floor(Math.random() * messages.length)];
 
-  const randomMessage = () => messages[(Math.floor(Math.random() * messages.length))];
+    const [greeting, setGreeting] = useState(messages[0]);
 
-  const [greeting, setGreeting] = useState(messages[0]);
-
-  return (
-    <div style="background-color: #4d1d9515; border-radius: 0.75em;">
-      <button onClick={() => setGreeting(randomMessage())} style="margin: 0.5rem;padding: 0.2rem 2rem; font-size: 1rem; font-weight: bold; border-radius: 0.75em;">
-        Click ME!
-      </button>
-      <h3 style="font-size: 1rem; display: inline; padding: 0.5rem; background-color: #fff78070; border-radius: 0.75em; color: black;" >{greeting}!  Ty for visiting my Island! <span style="font-family: 'Segoe UI Emoji';">🏝</span></h3>
-    </div>
-  );
+    return (
+        <div style="background-color: #4d1d9515; border-radius: 0.75em; padding: 0.5rem; text-align: center;">
+            <button onClick={() => setGreeting(randomMessage())} style="color: #4c1d95; padding: 0.5rem 0.5rem; font-size: 1rem; font-weight: bold; border-radius: 0.75rem;">
+                👉 PRESS ME! 👈
+            </button>
+            <p style="margin-bottom: 0;font-size: 1rem; display: block; padding: 0.5rem; background-color: #fff78070; border-radius: 0.75rem; color: black; font-weight: bold;">
+                {greeting}! Ty for visiting my Island! <span style="font-family: 'Segoe UI Emoji'; background-color: white;">🏝</span>
+            </p>
+        </div>
+    );
 }
