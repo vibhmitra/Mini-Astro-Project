7c968fd42a837c2577bf6c7310c52efc294f6d7c
vibhmitra <vibhanshumitra@gmail.com>
2024-09-18 10:43:32 +0530

    Added Sitemap 🎉

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,10 @@ import { defineConfig } from 'astro/config';
 
 import preact from "@astrojs/preact";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://miniastro.netlify.app",
-  integrations: [preact()]
+  integrations: [preact(), sitemap()]
 });
